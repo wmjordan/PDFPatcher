7833f29d9a62797bb81fbd676358a6336578ef9b
WMJ <wmj@live.cn>
2022-05-17 14:51:01 +0800

    ！调整常用文本模式的顺序，增加全英文大写的模式

--- App/Functions/Editor/Commands/QuickSelectCommand.cs
+++ App/Functions/Editor/Commands/QuickSelectCommand.cs
@@ -13,25 +13,34 @@ namespace PDFPatcher.Functions.Editor
 		const string __D = __S + @"[\.． ]";
 		const string __ND = __N + __D;
 		const string __NN = @"(?:\s*[^0-9０-９\.．一二三四五六七八九十〇]|$)";
-		static readonly MatchPattern[] __commands = {
-			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "(?:部分|部)|^" + __S + "part" + __S + "[0-9]", false, false, true){ Name =  "“第N部分”" },
-			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "[篇卷]|^" + __S + "(?:volume|vol)" + __S + "[0-9]", false, false, true){ Name =  "“第N篇”或“第N卷”" },
-			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "章|^" + __S + "chapter" + __S + "[0-9]", false, false, true){ Name =  "“第N章”" },
-			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "节|^" + __S + "section" + __S + "[0-9]", false, false, true){ Name =  "“第N节”" },
+		const string __UC = "[A-Z]+";
+		const string __OD = @"(?:[0-9]+\.?)?";
+
+		static readonly MatchPattern[] __Patterns = {
 			new MatchPattern ("^" + __S + __ND + "?" + __NN, true, false, true){ Name =  "“N.”模式" },
 			new MatchPattern ("^" + __S + __ND + __ND + "?" + __NN, true, false, true){ Name =  "“N.N”模式" },
 			new MatchPattern ("^" + __S + __ND + __ND + __ND + "?" + __NN, true, false, true){ Name =  "“N.N.N”模式" },
 			new MatchPattern ("^" + __S + __ND + __ND + __ND + __ND + "?" + __NN, true, false, true){ Name =  "“N.N.N.N”模式" },
-			new MatchPattern ("^" + __S + __ND + __ND + __ND + __ND + __ND + "?" + __NN, true, false, true){ Name =  "“N.N.N.N.N”模式" }
+			new MatchPattern ("^" + __S + __ND + __ND + __ND + __ND + __ND + "?" + __NN, true, false, true){ Name =  "“N.N.N.N.N”模式" },
+			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "(?:部分|部)|^" + __S + "part" + __S + "[0-9]", false, false, true){ Name =  "“第N部分”" },
+			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "[篇卷]|^" + __S + "(?:volume|vol)" + __S + "[0-9]", false, false, true){ Name =  "“第N篇”或“第N卷”" },
+			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "章|^" + __S + "chapter" + __S + "[0-9]", false, false, true){ Name =  "“第N章”" },
+			new MatchPattern ("^" + __S + "第" + __S + __N + __S + "节|^" + __S + "section" + __S + "[0-9]", false, false, true){ Name =  "“第N节”" },
+			new MatchPattern ("^(?:" + __S + __OD + __S + __UC + __S + ")+", true, true, true){ Name =  "全英文大写" },
 		};
 		internal static void RegisterCommands(CommandRegistry<Controller> registry) {
-			foreach (var item in __commands) {
+			foreach (var item in __Patterns) {
 				registry.Register(new QuickSelectCommand(item), item.Name);
 			}
 		}
 		internal static void RegisterMenuItems(ToolStripItemCollection container) {
-			foreach (var item in __commands) {
+			foreach (var item in __Patterns) {
 				container.Add(new ToolStripMenuItem(item.Name) { Name = item.Name });
+			}
+		}
+		internal static void RegisterMenuItemsWithPattern(ToolStripItemCollection container) {
+			foreach (var item in __Patterns) {
+				container.Add(new ToolStripMenuItem(item.Name) { Name = item.Name, Tag = item });
 			}
 		}
 
