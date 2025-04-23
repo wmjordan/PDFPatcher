156044c274b8fdfb9ba0773cf0ec58e1de21c8dd
WMJ <wmj@live.cn>
2025-04-23 20:02:30 +0800

    －页码标签缺少前缀导致程序崩溃

--- App/Processor/Mupdf/PageLabelCollection.cs
+++ App/Processor/Mupdf/PageLabelCollection.cs
@@ -131,7 +131,7 @@ namespace MuPDF
 		public PageLabel(int pageNumber, int startAt, string prefix, PageLabelStyle numericStyle) {
 			FromPageNumber = pageNumber;
 			StartAt = startAt;
-			Prefix = prefix;
+			Prefix = prefix ?? String.Empty;
 			NumericStyle = numericStyle;
 		}
 
@@ -163,6 +163,7 @@ namespace MuPDF
 	public enum PageLabelStyle : byte
 	{
 		Default = (byte)'-',
+		DigitN = (byte)'D',
 		Digit = (byte)'d',
 		UpperRoman = (byte)'R',
 		LowerRoman = (byte)'r',
