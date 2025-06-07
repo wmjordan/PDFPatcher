1789d6d998410c601eb1a0b3c185ac7c637c37f1
WMJ <wmj@live.cn>
2025-06-07 19:58:57 +0800

    ！重构书签管理器，将 iText 相关的方法放置到单独的文件

--- App/Processor/OutlineManager.cs
+++ App/Processor/OutlineManager.cs
@@ -1,255 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
-using iTextSharp.text.pdf;
 using PDFPatcher.Common;
 using PDFPatcher.Model;
 
 namespace PDFPatcher.Processor
 {
 	/// <summary>A class which manages outlines (bookmarks) of PDF documents.</summary>
-	static class OutlineManager
+	static partial class OutlineManager
 	{
 		static readonly char[] __fullWidthNumbers = "０１２３４５６７８９".ToCharArray();
 		static readonly char[] __halfWidthNumbers = "0123456789".ToCharArray();
 		static readonly char[] __cmdIdentifiers = ['=', '﹦', '＝', ':', '：'];
 		static readonly char[] __pageLabelSeparators = [';', '；', ',', '，', ' '];
-
-		static void BookmarkDepth(PdfReader reader, Dictionary<string, PdfObject> names, PdfActionExporter exporter, PdfDictionary outline, Dictionary<int, int> pageRefMap, XmlWriter target) {
-			while (outline != null) {
-				target.WriteStartElement(Constants.Bookmark);
-
-				target.WriteAttributeString(Constants.BookmarkAttributes.Title,
-					StringHelper.ReplaceControlAndBomCharacters(outline.GetAsString(PdfName.TITLE).Decode(AppContext.Encodings.BookmarkEncoding))
-					);
-
-				var color = outline.Locate<PdfArray>(PdfName.C);
-				DocInfoExporter.ExportColor(color, target);
-
-				var style = outline.Locate<PdfNumber>(PdfName.F);
-				if (style != null) {
-					int f = style.IntValue & 0x03;
-					if (f > 0) {
-						target.WriteAttributeString(Constants.BookmarkAttributes.Style, Constants.BookmarkAttributes.StyleType.Names[f]);
-					}
-				}
-
-				if (outline.Get(PdfName.COUNT) is PdfNumber count) {
-					target.WriteAttributeString(Constants.BookmarkAttributes.Open, count.IntValue < 0 ? Constants.Boolean.False : Constants.Boolean.True);
-				}
-
-				var dest = outline.Locate<PdfObject>(PdfName.DEST);
-				if (dest != null) {
-					exporter.ExportGotoAction(dest, names, target, pageRefMap);
-				}
-				else {
-					exporter.ExportAction(outline.Locate<PdfDictionary>(PdfName.A), names, pageRefMap, target);
-				}
-				var first = outline.Locate<PdfDictionary>(PdfName.FIRST);
-				if (first != null) {
-					BookmarkDepth(reader, names, exporter, first, pageRefMap, target);
-				}
-				outline = outline.Locate<PdfDictionary>(PdfName.NEXT);
-				target.WriteEndElement();
-			}
-		}
-
-		/// <summary>
-		/// 从 PDF 导出书签为 XML 元素。
-		/// </summary>
-		public static XmlElement GetBookmark(PdfReader reader, UnitConverter unitConverter) {
-			var catalog = reader.Catalog;
-			var outlines = catalog.Locate<PdfDictionary>(PdfName.OUTLINES);
-			if (outlines == null)
-				return null;
-			if (unitConverter == null) {
-				throw new NullReferenceException("unitConverter");
-			}
-			var pages = reader.GetPageRefMapper();
-			var doc = new XmlDocument();
-			doc.AppendElement(Constants.DocumentBookmark);
-			var names = reader.GetNamedDestinations();
-			using (var w = doc.DocumentElement.CreateNavigator().AppendChild()) {
-				var a = new PdfActionExporter(unitConverter);
-				BookmarkDepth(
-					reader,
-					names,
-					a,
-					(PdfDictionary)PdfReader.GetPdfObjectRelease(outlines.Get(PdfName.FIRST)),
-					pages,
-					w);
-			}
-			return doc.DocumentElement;
-		}
-
-		private static Object[] CreateOutlines(PdfWriter writer, PdfIndirectReference parent, XmlElement kids, int maxPageNumber, bool namedAsNames) {
-			var bookmarks = kids.SelectNodes(Constants.Bookmark);
-			var refs = new PdfIndirectReference[bookmarks.Count];
-			for (int k = 0; k < refs.Length; ++k) {
-				refs[k] = writer.PdfIndirectReference;
-			}
-
-			int ptr = 0;
-			int count = 0;
-			foreach (XmlElement child in bookmarks) {
-				object[] lower = null;
-				if (child.SelectSingleNode(Constants.Bookmark) != null)
-					lower = CreateOutlines(writer, refs[ptr], child, maxPageNumber, namedAsNames);
-				var outline = new PdfDictionary();
-				++count;
-				if (lower != null) {
-					outline.Put(PdfName.FIRST, (PdfIndirectReference)lower[0]);
-					outline.Put(PdfName.LAST, (PdfIndirectReference)lower[1]);
-					int n = (int)lower[2];
-					// 默认关闭书签
-					if (child.GetAttribute(Constants.BookmarkAttributes.Open) != Constants.Boolean.True) {
-						outline.Put(PdfName.COUNT, -n);
-					}
-					else {
-						outline.Put(PdfName.COUNT, n);
-						count += n;
-					}
-				}
-				outline.Put(PdfName.PARENT, parent);
-				if (ptr > 0)
-					outline.Put(PdfName.PREV, refs[ptr - 1]);
-				if (ptr < refs.Length - 1)
-					outline.Put(PdfName.NEXT, refs[ptr + 1]);
-				outline.Put(PdfName.TITLE, child.GetAttribute(Constants.BookmarkAttributes.Title));
-				DocInfoImporter.ImportColor(child, outline);
-				var style = child.GetAttribute(Constants.BookmarkAttributes.Style);
-				if (String.IsNullOrEmpty(style) == false) {
-					int bits = Array.IndexOf(Constants.BookmarkAttributes.StyleType.Names, style);
-					if (bits == -1) {
-						bits = 0;
-					}
-					if (bits != 0)
-						outline.Put(PdfName.F, bits);
-				}
-				DocInfoImporter.ImportAction(writer, outline, child, maxPageNumber, namedAsNames);
-				writer.AddToBody(outline, refs[ptr]);
-				++ptr;
-			}
-			return [refs[0], refs[refs.Length - 1], count];
-		}
-
-		internal static PdfIndirectReference WriteOutline(PdfWriter writer, XmlElement bookmarks, int maxPageNumber) {
-			if (bookmarks == null || bookmarks.SelectSingleNode(Constants.Bookmark) == null) {
-				return null;
-			}
-			var top = new PdfDictionary();
-			var topRef = writer.PdfIndirectReference;
-			var kids = CreateOutlines(writer, topRef, bookmarks, maxPageNumber, false);
-			top.Put(PdfName.TYPE, PdfName.OUTLINES);
-			top.Put(PdfName.FIRST, (PdfIndirectReference)kids[0]);
-			top.Put(PdfName.LAST, (PdfIndirectReference)kids[1]);
-			top.Put(PdfName.COUNT, (int)kids[2]);
-			writer.AddToBody(top, topRef);
-			writer.ExtraCatalog.Put(PdfName.OUTLINES, topRef);
-			return topRef;
-		}
-
-		internal static void KillOutline(PdfReader source) {
-			var catalog = source.Catalog;
-			var o = catalog.Get(PdfName.OUTLINES);
-			if (o == null) {
-				return;
-			}
-			if (o != null) {
-				var outlines = o as PRIndirectReference;
-				OutlineTravel(outlines);
-				PdfReader.KillIndirect(outlines);
-			}
-			catalog.Remove(PdfName.OUTLINES);
-			PdfReader.KillIndirect(catalog.Get(PdfName.OUTLINES));
-			if (PdfName.USEOUTLINES.Equals(catalog.GetAsName(PdfName.PAGEMODE))) {
-				catalog.Remove(PdfName.PAGEMODE);
-			}
-		}
-
-		private static void OutlineTravel(PRIndirectReference outline) {
-			while (outline != null) {
-				var outlineR = (PdfDictionary)PdfReader.GetPdfObjectRelease(outline);
-				PdfReader.KillIndirect(outline);
-				if (outlineR != null) {
-					var first = (PRIndirectReference)outlineR.Get(PdfName.FIRST);
-					if (first != null) {
-						OutlineTravel(first);
-					}
-					PdfReader.KillIndirect(outlineR.Get(PdfName.DEST));
-					PdfReader.KillIndirect(outlineR.Get(PdfName.A));
-					outline = (PRIndirectReference)outlineR.Get(PdfName.NEXT);
-				}
-				else {
-					outline = null;
-				}
-			}
-		}
-
-		public static string EscapeBinaryString(string s) {
-			var buf = StringBuilderCache.Acquire();
-			var cc = s.ToCharArray();
-			int len = cc.Length;
-			for (int k = 0; k < len; ++k) {
-				char c = cc[k];
-				if (c < ' ') {
-					buf.Append('\\');
-					int v = c;
-					var octal = "";
-					do {
-						int x = v % 8;
-						octal = x.ToText() + octal;
-						v /= 8;
-					} while (v > 0);
-					buf.Append(octal.PadLeft(3, '0'));
-				}
-				else if (c == '\\') {
-					buf.Append("\\\\");
-				}
-				else {
-					buf.Append(c);
-				}
-			}
-			return StringBuilderCache.GetStringAndRelease(buf);
-		}
-
-		public static string UnescapeBinaryString(String s) {
-			var buf = StringBuilderCache.Acquire();
-			var cc = s.ToCharArray();
-			int len = cc.Length;
-			for (int k = 0; k < len; ++k) {
-				char c = cc[k];
-				if (c != '\\') {
-					buf.Append(c);
-					continue;
-				}
-				if (++k >= len) {
-					buf.Append('\\');
-					break;
-				}
-				c = cc[k];
-				if (c < '0' || c > '7') {
-					buf.Append(c);
-					continue;
-				}
-				int n = c - '0';
-				++k;
-				for (int j = 0; j < 2 && k < len; ++j) {
-					c = cc[k];
-					if (c < '0' || c > '7') {
-						break;
-					}
-					++k;
-					n = n * 8 + c - '0';
-				}
-				--k;
-				buf.Append((char)n);
-			}
-			return StringBuilderCache.GetStringAndRelease(buf);
-		}
 
 		internal static void ImportSimpleBookmarks(TextReader source, PdfInfoXmlDocument target) {
 			string cmd, cmdData, s, title, indentString = "\t", pnText;
@@ -306,7 +71,7 @@ namespace PDFPatcher.Processor
 								continue;
 							}
 							int pn;
-							if (l[0].TryParse(out pn) == false || pn < 1) {
+							if (!l[0].TryParse(out pn) || pn < 1) {
 								Tracker.TraceMessage(Constants.PageLabels + "格式不正确：起始页码应为正整数。");
 								continue;
 							}
@@ -320,7 +85,7 @@ namespace PDFPatcher.Processor
 							var pl = target.CreateElement(Constants.PageLabelsAttributes.Style) as XmlElement;
 							pl.SetAttribute(Constants.PageLabelsAttributes.PageNumber, pn.ToText());
 							pl.SetAttribute(Constants.PageLabelsAttributes.Style, style);
-							if (String.IsNullOrEmpty(prefix) == false) {
+							if (!String.IsNullOrEmpty(prefix)) {
 								pl.SetAttribute(Constants.PageLabelsAttributes.Prefix, prefix);
 							}
 							pageLabels.AppendChild(pl);
@@ -340,7 +105,7 @@ namespace PDFPatcher.Processor
 					indent++;
 				}
 				var m = pattern.Match(s, p);
-				if (m.Success == false) {
+				if (!m.Success) {
 					continue;
 				}
 				title = m.Groups[1].Value;
@@ -376,7 +141,7 @@ namespace PDFPatcher.Processor
 					currentBookmark.ParentNode.AppendChild(bookmark);
 				}
 				bookmark.Title = title;
-				if (isOpen == false) {
+				if (!isOpen) {
 					bookmark.IsOpen = false;
 				}
 				if (pageNum > 0) {
---
+++ App/Processor/OutlineManager.iText.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using iTextSharp.text.pdf;
+using PDFPatcher.Common;
+using PDFPatcher.Model;
+
+namespace PDFPatcher.Processor
+{
+	partial class OutlineManager
+	{
+		/// <summary>
+		/// 从 PDF 导出书签为 XML 元素。
+		/// </summary>
+		public static XmlElement GetBookmark(PdfReader reader, UnitConverter unitConverter) {
+			var catalog = reader.Catalog;
+			var outlines = catalog.Locate<PdfDictionary>(PdfName.OUTLINES);
+			if (outlines == null)
+				return null;
+			if (unitConverter == null) {
+				throw new NullReferenceException("unitConverter");
+			}
+			var pages = reader.GetPageRefMapper();
+			var doc = new XmlDocument();
+			doc.AppendElement(Constants.DocumentBookmark);
+			var names = reader.GetNamedDestinations();
+			using (var w = doc.DocumentElement.CreateNavigator().AppendChild()) {
+				var a = new PdfActionExporter(unitConverter);
+				BookmarkDepth(
+					reader,
+					names,
+					a,
+					(PdfDictionary)PdfReader.GetPdfObjectRelease(outlines.Get(PdfName.FIRST)),
+					pages,
+					w);
+			}
+			return doc.DocumentElement;
+		}
+
+		static void BookmarkDepth(PdfReader reader, Dictionary<string, PdfObject> names, PdfActionExporter exporter, PdfDictionary outline, Dictionary<int, int> pageRefMap, XmlWriter target) {
+			while (outline != null) {
+				target.WriteStartElement(Constants.Bookmark);
+
+				target.WriteAttributeString(Constants.BookmarkAttributes.Title,
+					StringHelper.ReplaceControlAndBomCharacters(outline.GetAsString(PdfName.TITLE).Decode(AppContext.Encodings.BookmarkEncoding))
+					);
+
+				var color = outline.Locate<PdfArray>(PdfName.C);
+				DocInfoExporter.ExportColor(color, target);
+
+				var style = outline.Locate<PdfNumber>(PdfName.F);
+				if (style != null) {
+					int f = style.IntValue & 0x03;
+					if (f > 0) {
+						target.WriteAttributeString(Constants.BookmarkAttributes.Style, Constants.BookmarkAttributes.StyleType.Names[f]);
+					}
+				}
+
+				if (outline.Get(PdfName.COUNT) is PdfNumber count) {
+					target.WriteAttributeString(Constants.BookmarkAttributes.Open, count.IntValue < 0 ? Constants.Boolean.False : Constants.Boolean.True);
+				}
+
+				var dest = outline.Locate<PdfObject>(PdfName.DEST);
+				if (dest != null) {
+					exporter.ExportGotoAction(dest, names, target, pageRefMap);
+				}
+				else {
+					exporter.ExportAction(outline.Locate<PdfDictionary>(PdfName.A), names, pageRefMap, target);
+				}
+				var first = outline.Locate<PdfDictionary>(PdfName.FIRST);
+				if (first != null) {
+					BookmarkDepth(reader, names, exporter, first, pageRefMap, target);
+				}
+				outline = outline.Locate<PdfDictionary>(PdfName.NEXT);
+				target.WriteEndElement();
+			}
+		}
+
+		private static Object[] CreateOutlines(PdfWriter writer, PdfIndirectReference parent, XmlElement kids, int maxPageNumber, bool namedAsNames) {
+			var bookmarks = kids.SelectNodes(Constants.Bookmark);
+			var refs = new PdfIndirectReference[bookmarks.Count];
+			for (int k = 0; k < refs.Length; ++k) {
+				refs[k] = writer.PdfIndirectReference;
+			}
+
+			int ptr = 0;
+			int count = 0;
+			foreach (XmlElement child in bookmarks) {
+				object[] lower = null;
+				if (child.SelectSingleNode(Constants.Bookmark) != null)
+					lower = CreateOutlines(writer, refs[ptr], child, maxPageNumber, namedAsNames);
+				var outline = new PdfDictionary();
+				++count;
+				if (lower != null) {
+					outline.Put(PdfName.FIRST, (PdfIndirectReference)lower[0]);
+					outline.Put(PdfName.LAST, (PdfIndirectReference)lower[1]);
+					int n = (int)lower[2];
+					// 默认关闭书签
+					if (child.GetAttribute(Constants.BookmarkAttributes.Open) != Constants.Boolean.True) {
+						outline.Put(PdfName.COUNT, -n);
+					}
+					else {
+						outline.Put(PdfName.COUNT, n);
+						count += n;
+					}
+				}
+				outline.Put(PdfName.PARENT, parent);
+				if (ptr > 0)
+					outline.Put(PdfName.PREV, refs[ptr - 1]);
+				if (ptr < refs.Length - 1)
+					outline.Put(PdfName.NEXT, refs[ptr + 1]);
+				outline.Put(PdfName.TITLE, child.GetAttribute(Constants.BookmarkAttributes.Title));
+				DocInfoImporter.ImportColor(child, outline);
+				var style = child.GetAttribute(Constants.BookmarkAttributes.Style);
+				if (!String.IsNullOrEmpty(style)) {
+					int bits = Array.IndexOf(Constants.BookmarkAttributes.StyleType.Names, style);
+					if (bits == -1) {
+						bits = 0;
+					}
+					if (bits != 0)
+						outline.Put(PdfName.F, bits);
+				}
+				DocInfoImporter.ImportAction(writer, outline, child, maxPageNumber, namedAsNames);
+				writer.AddToBody(outline, refs[ptr]);
+				++ptr;
+			}
+			return [refs[0], refs[refs.Length - 1], count];
+		}
+
+		internal static PdfIndirectReference WriteOutline(PdfWriter writer, XmlElement bookmarks, int maxPageNumber) {
+			if (bookmarks == null || bookmarks.SelectSingleNode(Constants.Bookmark) == null) {
+				return null;
+			}
+			var top = new PdfDictionary();
+			var topRef = writer.PdfIndirectReference;
+			var kids = CreateOutlines(writer, topRef, bookmarks, maxPageNumber, false);
+			top.Put(PdfName.TYPE, PdfName.OUTLINES);
+			top.Put(PdfName.FIRST, (PdfIndirectReference)kids[0]);
+			top.Put(PdfName.LAST, (PdfIndirectReference)kids[1]);
+			top.Put(PdfName.COUNT, (int)kids[2]);
+			writer.AddToBody(top, topRef);
+			writer.ExtraCatalog.Put(PdfName.OUTLINES, topRef);
+			return topRef;
+		}
+
+		internal static void KillOutline(PdfReader source) {
+			var catalog = source.Catalog;
+			var o = catalog.Get(PdfName.OUTLINES);
+			if (o == null) {
+				return;
+			}
+			if (o != null) {
+				var outlines = o as PRIndirectReference;
+				OutlineTravel(outlines);
+				PdfReader.KillIndirect(outlines);
+			}
+			catalog.Remove(PdfName.OUTLINES);
+			PdfReader.KillIndirect(catalog.Get(PdfName.OUTLINES));
+			if (PdfName.USEOUTLINES.Equals(catalog.GetAsName(PdfName.PAGEMODE))) {
+				catalog.Remove(PdfName.PAGEMODE);
+			}
+		}
+
+		private static void OutlineTravel(PRIndirectReference outline) {
+			while (outline != null) {
+				var outlineR = (PdfDictionary)PdfReader.GetPdfObjectRelease(outline);
+				PdfReader.KillIndirect(outline);
+				if (outlineR != null) {
+					var first = (PRIndirectReference)outlineR.Get(PdfName.FIRST);
+					if (first != null) {
+						OutlineTravel(first);
+					}
+					PdfReader.KillIndirect(outlineR.Get(PdfName.DEST));
+					PdfReader.KillIndirect(outlineR.Get(PdfName.A));
+					outline = (PRIndirectReference)outlineR.Get(PdfName.NEXT);
+				}
+				else {
+					outline = null;
+				}
+			}
+		}
+	}
+}
