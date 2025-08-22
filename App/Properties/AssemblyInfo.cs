95d81226753467d86c7230958320594462eeca89
WMJ <wmj@live.cn>
2025-08-22 11:36:35 +0800

    －重新编译MuPDFLib，修复不兼容Windows7的问题

--- App/Properties/AssemblyInfo.cs
+++ App/Properties/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("e8969c5f-a5fe-4dce-a08b-555686483732")]
 
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.2.4660")]
+[assembly: AssemblyFileVersion("1.1.3.4662")]
