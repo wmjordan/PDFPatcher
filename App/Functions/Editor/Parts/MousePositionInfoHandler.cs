ab14d6d87865e14514e41d276a41fc7c1d8075f1
WMJ <wmj@live.cn>
2025-08-26 20:25:21 +0800

    ！重构代码，将鼠标对象信息移到到单独的文件

---
+++ App/Functions/Editor/Parts/MousePositionInfoHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+using PDFPatcher.Common;
+
+namespace PDFPatcher.Functions.Editor.Parts
+{
+	sealed class MousePositionInfoHandler
+	{
+		readonly Label _Label;
+		readonly ViewerControl _ViewerBox;
+
+		public MousePositionInfoHandler(Label label, ViewerControl viewerBox) {
+			_Label = label;
+			_ViewerBox = viewerBox;
+			viewerBox.MouseMove += HandleViewerBoxMouseMove;
+		}
+
+		void HandleViewerBoxMouseMove(object sender, MouseEventArgs e) {
+			if (_ViewerBox.FirstPage == 0) {
+				return;
+			}
+			var l = e.Location;
+			var p = _ViewerBox.TransposeClientToPagePosition(l.X, l.Y);
+			if (p.Page == 0) {
+				return;
+			}
+			var ti = _ViewerBox.FindTextLines(p);
+			var t = ti.ToString();
+			_Label.Text = string.Concat("页面：",
+				p.Page,
+				"; 位置：",
+				Math.Round(p.PageX, 2),
+				" * ",
+				Math.Round(p.PageY, 2),
+				ti.Spans.HasContent() ? String.Concat("; 字体：", String.Join(";", ti.GetFontNames()), " ", ti.Spans[0].Size) : null,
+				t != null ? "; 文本：" : null,
+				t);
+		}
+	}
+}
--- App/Functions/EditorControl.cs
+++ App/Functions/EditorControl.cs
@@ -47,6 +47,7 @@ namespace PDFPatcher.Functions
 			new Editor.Parts.BookmarkInViewSynchronizer(_BookmarkBox, _ViewerBox);
 			new Editor.Parts.BookmarkTitleEditHandler(_controller);
 			new Editor.Parts.CurrentPageBoxHandler(_CurrentPageBox, _ViewerBox);
+			new Editor.Parts.MousePositionInfoHandler(_PageInfoBox, _ViewerBox);
 			ListRecentFiles = _OpenButton_DropDownOpening;
 			RecentFileItemClicked = _OpenButton_DropDownItemClicked;
 			var s = this.GetDpiScale();
@@ -127,27 +128,6 @@ namespace PDFPatcher.Functions
 			//	}
 			//};
 			//_ViewerBox.MouseMode = MouseMode.Selection;
-			_ViewerBox.MouseMove += (s, args) => {
-				if (_ViewerBox.FirstPage == 0) {
-					return;
-				}
-				var l = args.Location;
-				var p = _ViewerBox.TransposeClientToPagePosition(l.X, l.Y);
-				if (p.Page == 0) {
-					return;
-				}
-				var ti = _ViewerBox.FindTextLines(p);
-				var t = ti.ToString();
-				_PageInfoBox.Text = string.Concat("页面：",
-					p.Page,
-					"; 位置：",
-					Math.Round(p.PageX, 2),
-					" * ",
-					Math.Round(p.PageY, 2),
-					ti.Spans.HasContent() ? String.Concat("; 字体：", String.Join(";", ti.GetFontNames()), " ", ti.Spans[0].Size) : null,
-					t != null ? "; 文本：" : null,
-					t);
-			};
 			_ViewerBox.MouseClick += _ViewBox_MouseClick;
 			_ViewerToolbar.Enabled = false;
 
