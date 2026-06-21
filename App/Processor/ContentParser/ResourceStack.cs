ca2fe7325cf4d1efdfd7fb53f35263cc901f90a4
WMJ <wmj@live.cn>
2026-06-21 11:15:09 +0800

    ！ResourceStack自动忽略空引用

--- App/Processor/ContentParser/ResourceStack.cs
+++ App/Processor/ContentParser/ResourceStack.cs
@@ -9,13 +9,20 @@ sealed class ResourceStack
 	readonly Stack<PdfDictionary> _stack = new();
 
 	public ResourceStack(PdfDictionary pageResources) {
-		_stack.Push(pageResources);
+		if (pageResources is not null) {
+			_stack.Push(pageResources);
+		}
 	}
 
 	public PdfDictionary Current => _stack.Count != 0 ? _stack.Peek() : null;
 
-	public void Push(PdfDictionary res) => _stack.Push(res);
-	public void Pop() => _stack.Pop();
+	public void Push(PdfDictionary res) {
+		if (res is not null) {
+			_stack.Push(res);
+		}
+	}
+
+	public PdfDictionary Pop() => _stack.Pop();
 
 	public PdfDictionary LookupResource(PdfNames resourceType, string resName) {
 		foreach (var layer in _stack) {
